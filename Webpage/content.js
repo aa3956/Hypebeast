f3ee20f3148c87bd28d3a02c855fa477a11c95c6
Ahmed Aziz <38285676+aa3956@users.noreply.github.com>
2019-03-07 16:39:41 +0000

    Add files via upload

---
+++ Webpage/content.js
@@ -0,0 +1,27 @@
+//Section control for all pages
+$(function() {
+  $.scrollify({
+    section : ".example-classname",
+  });
+});
+
+//Slideshow control for fashion page
+$(document).ready(function() {
+    $('#Carousel').carousel({
+        interval: 5000
+    })
+});
+
+//latest and popular control for videos page
+$(document).ready(function() {
+    $('.collapse').on('show.bs.collapse', function() {
+        var id = $(this).attr('id');
+        $('a[href="#' + id + '"]').closest('.panel-heading').addClass('active-faq');
+        $('a[href="#' + id + '"] .panel-title span').html('<i class="glyphicon glyphicon-minus"></i>');
+    });
+    $('.collapse').on('hide.bs.collapse', function() {
+        var id = $(this).attr('id');
+        $('a[href="#' + id + '"]').closest('.panel-heading').removeClass('active-faq');
+        $('a[href="#' + id + '"] .panel-title span').html('<i class="glyphicon glyphicon-plus"></i>');
+    });
+});
